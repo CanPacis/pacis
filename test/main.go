2873a81394d7a00e6eb227b965aabe7a6d781d35
Muhammed Ali CAN <tmwwdw@gmail.com>
2024-07-21 23:02:08 +0300

    refactor(test): use std binder for theming

--- test/main.go
+++ test/main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"net/http"
 
+	"github.com/CanPacis/pacis/std"
 	test "github.com/CanPacis/pacis/test/cases"
 	"github.com/CanPacis/pacis/test/views"
 	"github.com/CanPacis/pacis/ui"
@@ -20,51 +21,21 @@ func main() {
 		ResourcePrefix: "public",
 	})
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		applyTheme(app, r)
+	mux.HandleFunc("GET /", std.ThemeMiddleware(app, func(w http.ResponseWriter, r *http.Request) {
 		views.Home(pages).Render(app.Context(r.Context()), w)
-	})
+	}))
 
 	mux.Handle("GET /public/",
 		http.StripPrefix("/public/", http.FileServerFS(app.ResourceProvider.FS(ui.MustSubFS(public, "public")))),
 	)
 
 	for _, page := range pages {
-		mux.HandleFunc("GET "+page.Path(), func(w http.ResponseWriter, r *http.Request) {
-			applyTheme(app, r)
+		mux.HandleFunc("GET "+page.Path(), std.ThemeMiddleware(app, func(w http.ResponseWriter, r *http.Request) {
 			views.TestPage(page).Render(app.Context(r.Context()), w)
-		})
+		}))
 	}
 
-	mux.HandleFunc("GET /web/theme", func(w http.ResponseWriter, r *http.Request) {
-		scheme := r.FormValue("color-scheme")
-
-		if len(scheme) > 0 {
-			http.SetCookie(w, &http.Cookie{
-				Path:  "/",
-				Name:  "color-scheme",
-				Value: scheme,
-			})
-		}
-
-		http.Redirect(w, r, r.Referer(), http.StatusTemporaryRedirect)
-	})
+	mux.HandleFunc("GET /web/theme", std.ColorSchemeHandler)
 
 	http.ListenAndServe(":8080", mux)
-}
-
-func applyTheme(app *ui.App, r *http.Request) {
-	cookie, err := r.Cookie("color-scheme")
-	if err != nil {
-		return
-	}
-
-	switch cookie.Value {
-	case ui.Dark.String():
-		app.Theme.ColorScheme = ui.Dark
-	case ui.Light.String():
-		app.Theme.ColorScheme = ui.Light
-	default:
-		app.Theme.ColorScheme = ui.Light
-	}
 }
