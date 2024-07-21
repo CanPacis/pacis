315e2ee16d1e402be547277b2c0a973c2c9850fc
Muhammed Ali CAN <tmwwdw@gmail.com>
2024-07-21 18:03:37 +0300

    feat(docs): scaffold documentation

--- docs/main.go
+++ docs/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"embed"
+
+	"github.com/CanPacis/pacis/docs/views"
+	pe "github.com/CanPacis/pacis/echo"
+	"github.com/CanPacis/pacis/ui"
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed public
+var public embed.FS
+
+func main() {
+	app := ui.NewApp(ui.AppOptions{
+		ResourcePrefix: "public",
+	})
+
+	e := echo.New()
+	e.Use(pe.ThemeMiddleware(app))
+	e.GET("/", func(c echo.Context) error {
+		return views.Home().Render(app.Context(c.Request().Context()), c.Response().Writer)
+	})
+	e.GET("/web/theme", pe.ColorSchemeHandler)
+
+	e.StaticFS("/public", app.ResourceProvider.FS(ui.MustSubFS(public, "public")))
+	e.Logger.Fatal(e.Start(":8081"))
+}
